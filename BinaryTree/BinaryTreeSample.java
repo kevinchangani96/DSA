67d86affd5f998bec97ede9717d88c1d6094df36
kevin changani <changanikevin96@gmail.com>
2023-08-25 09:14:06 +0530

    Spiralorder print binary Tree Using deque

--- BinaryTree/BinaryTreeSample.java
+++ BinaryTree/BinaryTreeSample.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import javax.print.DocFlavor;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -19,15 +21,29 @@ public class BinaryTreeSample {
         r.right.left = new Node(60);
         r.right.right = new Node(70);
 
-        tree.printlevelorder();
-        System.out.println();
-        tree.preorderprint();
-        System.out.println();
-        tree.inorderprint();
-        System.out.println();
-        tree.postorderprint();
-        System.out.println();
+//        tree.printlevelorder();
+//        System.out.println();
+//        tree.preorderprint();
+//        System.out.println();
+//        tree.inorderprint();
+//        System.out.println();
+//        tree.postorderprint();
+//        System.out.println();
+        System.out.println(tree.countnode(tree.root));
+        tree.nthnode(tree.root, 5);
+        System.out.println(tree.findnode(tree.root, 10));
+        System.out.println(heightoftree(tree.root));
+        tree.PrintSprialLevel();
 
+    }
+
+    private static int heightoftree(Node root) {
+        if (root == null) {
+            return -1;
+        }
+        int leftheight = heightoftree(root.left);
+        int rightheight = heightoftree(root.right);
+        return 1 + Math.max(leftheight, rightheight);
     }
 
 
@@ -35,6 +51,7 @@ public class BinaryTreeSample {
 
 class tree {
     static Node root;
+    static int count = 0;
 
     public static void printlevelorder() {
         System.out.println("Level order Binary Tree");
@@ -42,7 +59,7 @@ class tree {
         queue.add(root);
         while (!queue.isEmpty()) {
             Node node = queue.poll();
-            System.out.print(node.data+ " ");
+            System.out.print(node.data + " ");
 
             if (node.left != null) {
                 queue.add(node.left);
@@ -59,10 +76,10 @@ class tree {
     }
 
     private static void preorderprintrecursion(Node root) {
-        if (root==null){
+        if (root == null) {
             return;
         }
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         preorderprintrecursion(root.left);
         preorderprintrecursion(root.right);
     }
@@ -73,11 +90,11 @@ class tree {
     }
 
     private static void inorderprintrecursion(Node root) {
-        if (root==null){
+        if (root == null) {
             return;
         }
         preorderprintrecursion(root.left);
-        System.out.print(root.data+ " ");
+        System.out.print(root.data + " ");
         preorderprintrecursion(root.right);
     }
 
@@ -87,16 +104,90 @@ class tree {
     }
 
     private static void postorderprintrecursion(Node root) {
-        if (root==null){
+        if (root == null) {
             return;
         }
         preorderprintrecursion(root.left);
         preorderprintrecursion(root.right);
-        System.out.print(root.data+ " ");
-    }
+        System.out.print(root.data + " ");
     }
 
 
+    public static int countnode(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countnode(root.left) + countnode(root.right);
+    }
+
+    public static void nthnode(Node root, int i) {
+        if (root == null) {
+            return;
+        }
+        nthnode(root.left, i);
+        count++;
+        if (count == i) {
+            System.out.println(root.data);
+            return;
+        }
+        nthnode(root.right, i);
+
+
+    }
+
+    public static boolean findnode(Node root, int i) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == i) {
+            return true;
+        }
+        return findnode(root.left, i) || findnode(root.right, i);
+    }
+
+    public static void PrintSprialLevel() {
+        Deque<Node> deque = new LinkedList<>();
+        boolean isRev = false;
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            if (isRev) {
+                int size = deque.size();
+                while (size > 0) {
+                    Node node = deque.removeLast();
+                    System.out.print(node.data + " ");
+                    if (node.right != null) {
+                        deque.addFirst(node.right);
+                    }
+                    if (node.left != null) {
+                        deque.addFirst(node.left);
+                    }
+                    size--;
+                    System.out.println();
+                }
+                isRev = !isRev;
+            } else {
+                int size = deque.size();
+                while (size > 0) {
+                    Node node = deque.removeFirst();
+                    System.out.print(node.data + " ");
+
+                    if (node.left != null) {
+                        deque.addLast(node.left);
+                    }
+                    if (node.right != null) {
+                        deque.addLast(node.right);
+                    }
+                    size--;
+                }
+                    System.out.println();
+
+
+            }
+
+        }
+
+    }
+}
 
 
 class Node {
